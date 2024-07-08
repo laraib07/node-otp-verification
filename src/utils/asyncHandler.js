2ebf0bdf4fc536a97038e9482f1e1821d38a9715
laraib07 <a.laraib@protonmail.com>
2024-07-08 23:21:26 +0530

    send response error as json

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,14 @@
+import { ApiResponse } from './index.js';
+
 export const asyncHandler = (requestHandler) => {
   return (req, res, next) => {
     Promise
       .resolve(requestHandler(req, res, next))
-      .catch((err) => next(err));
+      .catch((err) => {
+        res
+          .status(err.statusCode)
+          .json(new ApiResponse(err.statusCode, err.error, err.message))
+        next(err)
+      });
   }
 }
